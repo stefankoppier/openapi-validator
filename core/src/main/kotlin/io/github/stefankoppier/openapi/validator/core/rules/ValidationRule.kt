ba8e4ea07ab59b3ad8c276639b642aaa702fdf14
stefankoppier <stefan.koppier@outlook.com>
2023-05-29 09:06:48 +0200

    Small refactorings

--- core/src/main/kotlin/io/github/stefankoppier/openapi/validator/core/rules/ValidationRule.kt
+++ core/src/main/kotlin/io/github/stefankoppier/openapi/validator/core/rules/ValidationRule.kt
@@ -17,27 +17,19 @@ abstract class ValidationRule<T : Any>(val group: RuleGroup) {
         return rule()
     }
 
-    fun <R : ValidationRule<T>> R.holds(message: (T?) -> String = { "Was supposed to hold for '$it' but did not" }, predicate: (T?) -> Boolean): R {
-        add {
-            ValidationResult.condition(ValidationFailure(group, message(it))) {
-                predicate(it)
+    fun <R : ValidationRule<T>> R.holds(message: (T?) -> String = { "Was supposed to hold for '$it' but did not" }, predicate: (T?) -> Boolean) =
+        apply {
+            add {
+                ValidationResult.condition(ValidationFailure(group, message(it))) { predicate(it) }
             }
         }
-        return this
-    }
 
     // TODO: cool to have, if we have a ValidationRule which is nullable, after required, we can return a non nullable ValidationRule
-    fun <R : ValidationRule<T>> R.required(): R {
-        return holds( { "Was required but is not given" } ) {
-            it != null
-        }
-    }
+    fun <R : ValidationRule<T>> R.required() =
+        holds( { "Was required but is not given" } ) { it != null }
 
-    fun <R : ValidationRule<T>> R.exactly(value: T?): R {
-        return holds( { "Was supposed to be '$value' but is '$it'" } ) {
-            it == value
-        }
-    }
+    fun <R : ValidationRule<T>> R.exactly(value: T?) =
+        holds( { "Was supposed to be '$value' but is '$it'" } ) { it == value }
 
     protected fun add(rule: (T?) -> ValidationResult): ValidationRule<T> {
         val copy = rules
--- core/src/main/kotlin/io/github/stefankoppier/openapi/validator/core/rules/primitives/IterableValidationRule.kt
+++ core/src/main/kotlin/io/github/stefankoppier/openapi/validator/core/rules/primitives/IterableValidationRule.kt
@@ -6,21 +6,18 @@ import io.github.stefankoppier.openapi.validator.core.rules.ValidationRule
 // TODO: add variants where we can pass rules instead of predicates
 abstract class IterableValidationRule<T>(group: RuleGroup) : ValidationRule<Iterable<T>>(group) {
 
-    fun <R : IterableValidationRule<T>> R.all(description: String = "", predicate: (T) -> Boolean): R {
-        return holds( { "All were supposed to match '$description' but not all did" } ) {
+    fun <R : IterableValidationRule<T>> R.all(description: String = "", predicate: (T) -> Boolean): R =
+        holds( { "All were supposed to match '$description' but not all did" } ) {
             it?.all(predicate) ?: true
         }
-    }
 
-    fun <R : IterableValidationRule<T>> R.any(description: String = "", predicate: (T) -> Boolean): R {
-        return holds( { "At least one was supposed to match '$description' but none did" } ) {
+    fun <R : IterableValidationRule<T>> R.any(description: String = "", predicate: (T) -> Boolean): R =
+        holds( { "At least one was supposed to match '$description' but none did" } ) {
             it?.any(predicate) ?: false
         }
-    }
 
-    fun <R : IterableValidationRule<T>> R.none(description: String = "", predicate: (T) -> Boolean): R {
-        return holds( { "None were supposed to match '$description' but at least one did" } ) {
+    fun <R : IterableValidationRule<T>> R.none(description: String = "", predicate: (T) -> Boolean): R =
+        holds( { "None were supposed to match '$description' but at least one did" } ) {
             it?.none(predicate) ?: false
         }
-    }
 }
