27f134b0419673abd8339922078a3ce8641761fd
stefankoppier <stefan.koppier@outlook.com>
2023-06-17 09:14:28 +0200

    Added EncodingsRuleTest

--- core/src/main/kotlin/io/github/stefankoppier/openapi/validator/core/rules/openapi/collections/EncodingsRule.kt
+++ core/src/main/kotlin/io/github/stefankoppier/openapi/validator/core/rules/openapi/collections/EncodingsRule.kt
@@ -7,6 +7,12 @@ import io.swagger.v3.oas.models.media.Encoding
 
 class EncodingsRule internal constructor(group: RuleGroup = RuleGroup.unknown()) : IterableValidationRule<Pair<String, Encoding>>(group) {
 
+    fun all(description: String = "", rule: EncodingRule.() -> EncodingRule) =
+        all { encoding ->
+            rule(EncodingRule(RuleGroup.named("encoding '${encoding.first}'", description, RuleGroup.Category.OBJECT, group)))
+                .validate(encoding.second)
+        }
+
     fun encoding(description: String = "", named: String, rule: EncodingRule.() -> EncodingRule) =
         apply {
             add { encodings ->
---
+++ core/src/test/kotlin/io/github/stefankoppier/openapi/validator/core/rules/openapi/collections/EncodingsRuleTest.kt
@@ -0,0 +1,56 @@
+package io.github.stefankoppier.openapi.validator.core.rules.openapi.collections
+
+import io.swagger.v3.oas.models.media.Encoding
+import org.assertj.core.api.Assertions.assertThat
+import kotlin.test.Test
+
+class EncodingsRuleTest {
+
+    @Test
+    fun `all succeeds`() {
+        val rule = EncodingsRule().all {
+            contentType { exactly("application/json") }
+        }
+
+        val result = rule.validate(encodings)
+        assertThat(result.isSuccess).isTrue()
+    }
+
+    @Test
+    fun `all fails`() {
+        val rule = EncodingsRule().all {
+            contentType { exactly("application/xml") }
+        }
+
+        val result = rule.validate(encodings)
+        assertThat(result.isFailure).isTrue()
+    }
+
+    @Test
+    fun `path succeeds`() {
+        val rule = EncodingsRule().encoding(named = "json") {
+            contentType { exactly("application/json") }
+        }
+
+        val result = rule.validate(encodings)
+        assertThat(result.isSuccess).isTrue()
+    }
+
+    @Test
+    fun `path fails`() {
+        val rule = EncodingsRule().encoding(named = "json") {
+             contentType { exactly("application/xml") }
+        }
+
+        val result = rule.validate(encodings)
+        assertThat(result.isFailure).isTrue()
+    }
+
+    companion object {
+        private val encodings = mapOf(
+            "json" to Encoding().apply {
+                contentType = "application/json"
+            }
+        ).toList()
+    }
+}
