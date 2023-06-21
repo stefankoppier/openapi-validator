7495233781ed93b40e5f14e38d6a5a58385bc727
stefankoppier <stefan.koppier@outlook.com>
2023-06-21 18:13:26 +0200

    Added EnumRule tests

---
+++ core/src/test/kotlin/io/github/stefankoppier/openapi/validator/core/rules/primitives/EnumRuleTest.kt
@@ -0,0 +1,44 @@
+package io.github.stefankoppier.openapi.validator.core.rules.primitives
+
+import io.github.stefankoppier.openapi.validator.core.ValidationFailure
+import io.github.stefankoppier.openapi.validator.core.assertThat
+import io.github.stefankoppier.openapi.validator.core.rules.RuleGroup
+import kotlin.test.Test
+
+class EnumRuleTest {
+
+    @Test
+    fun `either succeeds`() {
+        val rule = enum {
+            either(Color.RED, Color.GREEN)
+        }
+
+        assertThat(rule.validate(Color.RED)).isSuccess()
+    }
+
+    @Test
+    fun `either fails`() {
+        val rule = enum {
+            either(Color.RED, Color.GREEN)
+        }
+
+        assertThat(rule.validate(Color.BLUE)).isFailure(
+            ValidationFailure(
+                RuleGroup.named("rule", "", RuleGroup.Category.FIELD),
+                "Was supposed to be one of 'RED, GREEN' but is 'BLUE'",
+            ),
+        )
+    }
+
+    companion object {
+        private enum class Color {
+            RED,
+            GREEN,
+            BLUE,
+        }
+
+        private fun enum(rule: EnumRule<Color>.() -> EnumRule<Color>): EnumRule<Color> {
+            return rule(EnumRule(RuleGroup.named("rule", "", RuleGroup.Category.FIELD)))
+        }
+    }
+}
