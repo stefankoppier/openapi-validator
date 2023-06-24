4d2c244da9718c62ed674005af1072b7885f0231
stefankoppier <stefan.koppier@outlook.com>
2023-06-24 13:32:31 +0200

    removed afterEvaluate

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,6 @@ dependencies {
 }
 
 allprojects {
-    repositories {
-        mavenCentral()
-    }
-
     plugins.withId("org.jetbrains.kotlin.jvm") {
         apply(plugin = "jvm-test-suite")
         apply(plugin = "jacoco")
@@ -94,66 +90,64 @@ allprojects {
         }
 
         if (project != rootProject) {
-            afterEvaluate {
-                val dokkaHtml by tasks.getting(DokkaTask::class)
-                val javadocJar by tasks.registering(Jar::class) {
-                    dependsOn(dokkaHtml)
-                    archiveClassifier.set("javadoc")
-                    from(dokkaHtml.outputDirectory)
-                }
+            val dokkaHtml by tasks.getting(DokkaTask::class)
+            val javadocJar by tasks.registering(Jar::class) {
+                dependsOn(dokkaHtml)
+                archiveClassifier.set("javadoc")
+                from(dokkaHtml.outputDirectory)
+            }
 
-                publishing {
-                    repositories {
-                        maven {
-                            name = "OSSRH"
-                            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            publishing {
+                repositories {
+                    maven {
+                        name = "OSSRH"
+                        url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
 
-                            credentials {
-                                username = properties["ossrhUsername"] as String
-                                password = properties["ossrhPassword"] as String
-                            }
+                        credentials {
+                            username = properties["ossrhUsername"] as String
+                            password = properties["ossrhPassword"] as String
                         }
                     }
+                }
 
-                    publications {
-                        create<MavenPublication>(project.name) {
-                            from(components["kotlin"])
-                            artifact(tasks.named("kotlinSourcesJar").get())
-                            artifact(javadocJar)
-                            groupId = rootProject.group.toString()
-                            artifactId = project.name
-                            version = rootProject.version.toString()
+                publications {
+                    create<MavenPublication>(project.name) {
+                        from(components["kotlin"])
+                        artifact(tasks.named("kotlinSourcesJar").get())
+                        artifact(javadocJar)
+                        groupId = rootProject.group.toString()
+                        artifactId = project.name
+                        version = rootProject.version.toString()
 
-                            pom {
-                                name.set("$group:${rootProject.name}-${project.name}")
-                                description.set(project.description)
-                                url.set("https://github.com/stefankoppier/openapi-validator")
-                                developers {
-                                    developer {
-                                        id.set("stefankoppier")
-                                        name.set("Stefan Koppier")
-                                        email.set("stefan.koppier@outlook.com")
-                                        url.set("https://github.com/stefankoppier")
-                                    }
+                        pom {
+                            name.set("$group:${rootProject.name}-${project.name}")
+                            description.set(project.description)
+                            url.set("https://github.com/stefankoppier/openapi-validator")
+                            developers {
+                                developer {
+                                    id.set("stefankoppier")
+                                    name.set("Stefan Koppier")
+                                    email.set("stefan.koppier@outlook.com")
+                                    url.set("https://github.com/stefankoppier")
                                 }
+                            }
 
-                                scm {
-                                    connection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
-                                    developerConnection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
-                                    url.set("https://github.com/stefankoppier/openapi-validator/tree/main")
-                                }
+                            scm {
+                                connection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
+                                developerConnection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
+                                url.set("https://github.com/stefankoppier/openapi-validator/tree/main")
+                            }
 
-                                licenses {
-                                    license {
-                                        name.set("The Apache License, Version 2.0")
-                                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                                    }
+                            licenses {
+                                license {
+                                    name.set("The Apache License, Version 2.0")
+                                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
                                 }
+                            }
 
-                                issueManagement {
-                                    system.set("GitHub")
-                                    url.set("https://github.com/stefankoppier/openapi-validator/issues")
-                                }
+                            issueManagement {
+                                system.set("GitHub")
+                                url.set("https://github.com/stefankoppier/openapi-validator/issues")
                             }
                         }
                     }
