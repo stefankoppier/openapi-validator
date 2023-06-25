80b9c7723b234eeb8d7d821ea1e1c1b597345cfa
stefankoppier <stefan.koppier@outlook.com>
2023-06-25 08:58:24 +0200

    Try to fix build in workflow

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     id("java-test-fixtures")
-//    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.jvm)
 }
 
 description = "Core module of the OpenAPI validator project."
 
-//dependencies {
-//    api(libs.swagger.models)
-//
-//    implementation(libs.swagger.parser)
-//
-//    testFixturesImplementation(libs.assertj.core)
-//
-//    integrationTestImplementation(libs.guava)
-//}
+dependencies {
+    api(libs.swagger.models)
+
+    implementation(libs.swagger.parser)
+
+    testFixturesImplementation(libs.assertj.core)
+
+    integrationTestImplementation(libs.guava)
+}
