d13de961a0f61405bd8f734ad47e7b5fd32aec78
stefankoppier <stefan.koppier@outlook.com>
2023-06-25 11:50:17 +0200

    Try to fix build in workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,48 +115,48 @@ allprojects {
                     }
                 }
 
-                publications {
-                    create<MavenPublication>(project.name) {
-                        from(components["kotlin"])
-                        artifact(tasks.named("kotlinSourcesJar").get())
-//                        artifact(javadocJar)
-                        groupId = rootProject.group.toString()
-                        artifactId = project.name
-                        version = rootProject.version.toString()
-
-                        pom {
-                            name.set("$group:${rootProject.name}-${project.name}")
-                            description.set(project.description)
-                            url.set("https://github.com/stefankoppier/openapi-validator")
-                            developers {
-                                developer {
-                                    id.set("stefankoppier")
-                                    name.set("Stefan Koppier")
-                                    email.set("stefan.koppier@outlook.com")
-                                    url.set("https://github.com/stefankoppier")
-                                }
-                            }
-
-                            scm {
-                                connection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
-                                developerConnection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
-                                url.set("https://github.com/stefankoppier/openapi-validator/tree/main")
-                            }
-
-                            licenses {
-                                license {
-                                    name.set("The Apache License, Version 2.0")
-                                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                                }
-                            }
-
-                            issueManagement {
-                                system.set("GitHub")
-                                url.set("https://github.com/stefankoppier/openapi-validator/issues")
-                            }
-                        }
-                    }
-                }
+//                publications {
+//                    create<MavenPublication>(project.name) {
+//                        from(components["kotlin"])
+//                        artifact(tasks.named("kotlinSourcesJar").get())
+////                        artifact(javadocJar)
+//                        groupId = rootProject.group.toString()
+//                        artifactId = project.name
+//                        version = rootProject.version.toString()
+//
+//                        pom {
+//                            name.set("$group:${rootProject.name}-${project.name}")
+//                            description.set(project.description)
+//                            url.set("https://github.com/stefankoppier/openapi-validator")
+//                            developers {
+//                                developer {
+//                                    id.set("stefankoppier")
+//                                    name.set("Stefan Koppier")
+//                                    email.set("stefan.koppier@outlook.com")
+//                                    url.set("https://github.com/stefankoppier")
+//                                }
+//                            }
+//
+//                            scm {
+//                                connection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
+//                                developerConnection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
+//                                url.set("https://github.com/stefankoppier/openapi-validator/tree/main")
+//                            }
+//
+//                            licenses {
+//                                license {
+//                                    name.set("The Apache License, Version 2.0")
+//                                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                                }
+//                            }
+//
+//                            issueManagement {
+//                                system.set("GitHub")
+//                                url.set("https://github.com/stefankoppier/openapi-validator/issues")
+//                            }
+//                        }
+//                    }
+//                }
             }
 
 //            signing {
