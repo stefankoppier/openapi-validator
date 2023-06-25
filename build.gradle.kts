56d1a09c1e9f80ab4b6f5873167bd1ce40fe18c9
stefankoppier <stefan.koppier@outlook.com>
2023-06-25 11:46:00 +0200

    Try to fix build in workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -95,72 +95,70 @@ allprojects {
         }
 
         if (project != rootProject) {
-//            afterEvaluate {
-//                val dokkaHtml by tasks.getting(DokkaTask::class)
-//                val javadocJar by tasks.registering(Jar::class) {
-//                    dependsOn(dokkaHtml)
-//                    archiveClassifier.set("javadoc")
-//                    from(dokkaHtml.outputDirectory)
-//                }
-//
-//                publishing {
-//                    repositories {
-//                        maven {
-//                            name = "OSSRH"
-//                            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-//
-//                            credentials {
-//                                username = properties["ossrhUsername"] as String
-//                                password = properties["ossrhPassword"] as String
-//                            }
-//                        }
-//                    }
-//
-//                    publications {
-//                        create<MavenPublication>(project.name) {
-//                            from(components["kotlin"])
-//                            artifact(tasks.named("kotlinSourcesJar").get())
-//                            artifact(javadocJar)
-//                            groupId = rootProject.group.toString()
-//                            artifactId = project.name
-//                            version = rootProject.version.toString()
-//
-//                            pom {
-//                                name.set("$group:${rootProject.name}-${project.name}")
-//                                description.set(project.description)
-//                                url.set("https://github.com/stefankoppier/openapi-validator")
-//                                developers {
-//                                    developer {
-//                                        id.set("stefankoppier")
-//                                        name.set("Stefan Koppier")
-//                                        email.set("stefan.koppier@outlook.com")
-//                                        url.set("https://github.com/stefankoppier")
-//                                    }
-//                                }
-//
-//                                scm {
-//                                    connection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
-//                                    developerConnection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
-//                                    url.set("https://github.com/stefankoppier/openapi-validator/tree/main")
-//                                }
-//
-//                                licenses {
-//                                    license {
-//                                        name.set("The Apache License, Version 2.0")
-//                                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                                    }
-//                                }
-//
-//                                issueManagement {
-//                                    system.set("GitHub")
-//                                    url.set("https://github.com/stefankoppier/openapi-validator/issues")
-//                                }
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//
+            val dokkaHtml by tasks.getting(DokkaTask::class)
+            val javadocJar by tasks.registering(Jar::class) {
+                dependsOn(dokkaHtml)
+                archiveClassifier.set("javadoc")
+                from(dokkaHtml.outputDirectory)
+            }
+
+            publishing {
+                repositories {
+                    maven {
+                        name = "OSSRH"
+                        url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+
+                        credentials {
+                            username = properties["ossrhUsername"] as String
+                            password = properties["ossrhPassword"] as String
+                        }
+                    }
+                }
+
+                publications {
+                    create<MavenPublication>(project.name) {
+                        from(components["kotlin"])
+                        artifact(tasks.named("kotlinSourcesJar").get())
+                        artifact(javadocJar)
+                        groupId = rootProject.group.toString()
+                        artifactId = project.name
+                        version = rootProject.version.toString()
+
+                        pom {
+                            name.set("$group:${rootProject.name}-${project.name}")
+                            description.set(project.description)
+                            url.set("https://github.com/stefankoppier/openapi-validator")
+                            developers {
+                                developer {
+                                    id.set("stefankoppier")
+                                    name.set("Stefan Koppier")
+                                    email.set("stefan.koppier@outlook.com")
+                                    url.set("https://github.com/stefankoppier")
+                                }
+                            }
+
+                            scm {
+                                connection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
+                                developerConnection.set("scm:git:git@github.com/stefankoppier/openapi-validator.git")
+                                url.set("https://github.com/stefankoppier/openapi-validator/tree/main")
+                            }
+
+                            licenses {
+                                license {
+                                    name.set("The Apache License, Version 2.0")
+                                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                                }
+                            }
+
+                            issueManagement {
+                                system.set("GitHub")
+                                url.set("https://github.com/stefankoppier/openapi-validator/issues")
+                            }
+                        }
+                    }
+                }
+            }
+
             signing {
                 val key = findProperty("signing.key") as String
                 val password = findProperty("signing.password") as String
